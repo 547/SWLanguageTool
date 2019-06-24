4eb434d3f455ef18ecf13d23ffbaf7edb942d005
Seven Wang <seven.wang@supernovachina.com>
2019-06-24 10:16:53 +0800

    Initial Commit

---
+++ SWLanguageTool/SWLanguageTool.h
@@ -0,0 +1,19 @@
+//
+//  SWLanguageTool.h
+//  SWLanguageTool
+//
+//  Created by Supernova SanDick SSD on 2019/6/24.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWLanguageTool.
+FOUNDATION_EXPORT double SWLanguageToolVersionNumber;
+
+//! Project version string for SWLanguageTool.
+FOUNDATION_EXPORT const unsigned char SWLanguageToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWLanguageTool/PublicHeader.h>
+
+
